9bf1bbd74588168d3c43429caca37c54aca6d388
Lovin <jean.l.ff@gmail.com>
2025-11-08 02:15:18 -0300

    Fix: Adiciona a URL correta do banco de dados

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # IMPORTANTE: Cole aqui a URL de conexão do seu NOVO banco de dados do Render
-DATABASE_URL = "SUA_URL_DO_POSTGRESQL_VAI_AQUI" 
+DATABASE_URL = "postgresql://ritmistas_db_user:1ohdtnoKV0hvdDXfzR85dCUA3fZcPzzB@dpg-d47cvmfdiees7399dt0g-a/ritmistas_db_data" 
 
 # Configuração do motor (Engine) do SQLAlchemy
 engine = create_engine(DATABASE_URL)
