bc990c2340cc3039a243c31f23eb575c8c260660
Lovin <jean.l.ff@gmail.com>
2025-11-24 19:45:12 -0300

    Fix: Atualiza models.py para Versao 3.0 com tabela de associacao

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # IMPORTANTE: Substitua pela URL do seu NOVO banco de dados no Render
 # Ex: postgresql://user:pass@host/dbname
-DATABASE_URL = "postgresql://ritmistas_db_a5cw_user:TG48FkQwEsC6fAWNDS8UtuDrAa5LEroa@dpg-d4ido0vpm1nc73fov6fg-a/ritmistas_db_a5cw"
+DATABASE_URL = "postgresql://ritmistas_db_paos_user:LTISbykM3dXNeK22Wb8IA59ofpqOXZ3T@dpg-d4idvc24d50c7383i4d0-a/ritmistas_db_paos"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
